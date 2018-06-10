ea7d5b060ded9c148d4db1b4ef0f9fcb67f7c195
Devon Scott <dscott@bluefission.com>
2018-06-10 16:17:35 -0400

    added api documentation

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,28 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// API Calls
+Route::get('/v1/goals', 'GoalController@index');
+Route::get('/v1/goals/{goal}', 'GoalController@show');
+Route::post('/v1/goals', 'GoalController@store');
+Route::put('/v1/goals/{goal}', 'GoalController@update');
+Route::delete('/v1/goals/{goal}', 'GoalController@delete');
+
+Route::get('/v1/tasks', 'TaskController@index');
+Route::get('/v1/tasks/{task}', 'TaskController@show');
+Route::post('/v1/tasks', 'TaskController@store');
+Route::put('/v1/tasks/{task}', 'TaskController@update');
+Route::delete('/v1/tasks/{task}', 'TaskController@delete');
+
+Route::get('/v1/logs', 'LogController@index');
+Route::get('/v1/logs/{log}', 'LogController@show');
+Route::post('/v1/logs', 'LogController@store');
+Route::put('/v1/logs/{log}', 'LogController@update');
+Route::delete('/v1/logs/{log}', 'LogController@delete');
+
+Route::get('/v1/kpi_types', 'KpiTypeController@index');
+Route::get('/v1/kpi_types/{kpi_type}', 'KpiTypeController@show');
+Route::post('/v1/kpi_types', 'KpiTypeController@store');
+Route::put('/v1/kpi_types/{kpi_type}', 'KpiTypeController@update');
+Route::delete('/v1/kpi_types/{kpi_type}', 'KpiTypeController@delete');
--- routes/web.php
+++ routes/web.php
@@ -18,28 +18,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-// API Calls
-Route::get('/api/goals', 'Api\GoalController@index');
-Route::get('/api/goals/{goal}', 'Api\GoalController@show');
-Route::post('/api/goals', 'Api\GoalController@store');
-Route::put('/api/goals/{goal}', 'Api\GoalController@update');
-Route::delete('/api/goals/{goal}', 'Api\GoalController@delete');
-
-Route::get('/api/tasks', 'Api\TaskController@index');
-Route::get('/api/tasks/{task}', 'Api\TaskController@show');
-Route::post('/api/tasks', 'Api\TaskController@store');
-Route::put('/api/tasks/{task}', 'Api\TaskController@update');
-Route::delete('/api/tasks/{task}', 'Api\TaskController@delete');
-
-Route::get('/api/logs', 'Api\LogController@index');
-Route::get('/api/logs/{log}', 'Api\LogController@show');
-Route::post('/api/logs', 'Api\LogController@store');
-Route::put('/api/logs/{log}', 'Api\LogController@update');
-Route::delete('/api/logs/{log}', 'Api\LogController@delete');
-
-Route::get('/api/kpi_types', 'Api\KpiTypeController@index');
-Route::get('/api/kpi_types/{kpi_type}', 'Api\KpiTypeController@show');
-Route::post('/api/kpi_types', 'Api\KpiTypeController@store');
-Route::put('/api/kpi_types/{kpi_type}', 'Api\KpiTypeController@update');
-Route::delete('/api/kpi_types/{kpi_type}', 'Api\KpiTypeController@delete');
